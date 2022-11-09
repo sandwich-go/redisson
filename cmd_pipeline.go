7221f682bfb0ebcfaa3cc586539a93dc52808873
祝黄清 <huangqing.zhu@centurygame.com>
2022-11-09 14:51:04 +0800

    feat: 增加RawCmdable接口

--- cmd_pipeline.go
+++ cmd_pipeline.go
@@ -13,4 +13,5 @@ type Pipeliner interface {
 	Exec(ctx context.Context) ([]interface{}, error)
 }
 
-func (c *client) Pipeline() Pipeliner { return c.cmdable.Pipeline() }
+func (c *client) Pipeline() Pipeliner     { return c.cmdable.Pipeline() }
+func (c *client) RawCmdable() interface{} { return c.cmdable.RawCmdable() }
