e00111e8d083148e1276e1193722e1fd22aaa4ae
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-01-19 11:25:16 +0800

    fix

--- redis.go
+++ redis.go
@@ -1,13 +1,14 @@
 package redisson
 
 import (
+	"github.com/sandwich-go/funnel"
 	"time"
 )
 
 type Cmdable interface {
 	Cache(ttl time.Duration) CacheCmdable
 	NewLocker(opts ...LockerOption) (Locker, error)
-	NewFunnel(key string, capacity, operations int64, seconds time.Duration) Funnel
+	NewFunnel(key string, capacity, operations int64, seconds time.Duration) funnel.Funnel
 	PoolStats() PoolStats
 	RegisterCollector(RegisterCollectorFunc)
 	Close() error
