45211e8470f8326c2e8eb8c648326211cb169dfd
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-07-19 18:19:28 +0800

    refactor: 注释

--- cmd_string.go
+++ cmd_string.go
@@ -15,104 +15,166 @@ type StringCmdable interface {
 type StringWriter interface {
 	// Append
 	// Available since: 2.0.0
-	// Time complexity: O(1). The amortized time complexity is O(1) assuming the appended value is small and the already present value is of any size, since the dynamic string library used by Redis will double the free space available on every reallocation.
+	// Time complexity: O(1). The amortized time complexity is O(1) assuming the appended value is small and the already present value is of any size,
+	//					since the dynamic string library used by Redis will double the free space available on every reallocation.
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the length of the string after the append operation.
 	Append(ctx context.Context, key, value string) IntCmd
 
 	// Decr
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the value of the key after decrementing it.
 	Decr(ctx context.Context, key string) IntCmd
 
 	// DecrBy
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the value of the key after decrementing it.
 	DecrBy(ctx context.Context, key string, decrement int64) IntCmd
 
 	// GetDel
 	// Available since: 6.2.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Bulk string reply: the value of the key.
+	//		- Nil reply: if the key does not exist or if the key's value type is not a string.
 	GetDel(ctx context.Context, key string) StringCmd
 
 	// GetEx
 	// Available since: 6.2.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// Options
+	// 	- EX seconds -- Set the specified expire time, in seconds.
+	// 	- PX milliseconds -- Set the specified expire time, in milliseconds.
+	// 	- EXAT timestamp-seconds -- Set the specified Unix time at which the key will expire, in seconds.
+	// 	- PXAT timestamp-milliseconds -- Set the specified Unix time at which the key will expire, in milliseconds.
+	// 	- PERSIST -- Remove the time to live associated with the key.
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Bulk string reply: the value of the key.
+	//		- Nil reply: if key does not exist.
 	GetEx(ctx context.Context, key string, expiration time.Duration) StringCmd
 
 	// GetSet
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Bulk string reply: the old value stored at the key.
+	//		- Nil reply: if key does not exist.
 	GetSet(ctx context.Context, key string, value any) StringCmd
 
 	// Incr
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the value of the key after the increment.
 	Incr(ctx context.Context, key string) IntCmd
 
 	// IncrBy
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the value of the key after the increment.
 	IncrBy(ctx context.Context, key string, value int64) IntCmd
 
 	// IncrByFloat
 	// Available since: 2.6.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the value of the key after the increment.
 	IncrByFloat(ctx context.Context, key string, value float64) FloatCmd
 
 	// MSet
 	// Available since: 1.0.1
 	// Time complexity: O(N) where N is the number of keys to set.
 	// ACL categories: @write @string @slow
+	// RESP2 / RESP3 Reply:
+	// 	- Simple string reply: always OK because MSET can't fail.
 	MSet(ctx context.Context, values ...any) StatusCmd
 
 	// MSetNX
 	// Available since: 1.0.1
 	// Time complexity: O(N) where N is the number of keys to set.
 	// ACL categories: @write @string @slow
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Integer reply: 0 if no key was set (at least one key already existed).
+	//		- Integer reply: 1 if all the keys were set.
 	MSetNX(ctx context.Context, values ...any) BoolCmd
 
 	// Set
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @slow
+	// Options
+	// 	- EX seconds -- Set the specified expire time, in seconds (a positive integer).
+	// 	- PX milliseconds -- Set the specified expire time, in milliseconds (a positive integer).
+	// 	- EXAT timestamp-seconds -- Set the specified Unix time at which the key will expire, in seconds (a positive integer).
+	// 	- PXAT timestamp-milliseconds -- Set the specified Unix time at which the key will expire, in milliseconds (a positive integer).
+	// 	- NX -- Only set the key if it does not already exist.
+	// 	- XX -- Only set the key if it already exists.
+	// 	- KEEPTTL -- Retain the time to live associated with the key.
+	// 	- GET -- Return the old string stored at key, or nil if key did not exist. An error is returned and SET aborted if the value stored at key is not a string.
+	// RESP2 Reply:
+	//	Any of the following:
+	//		- Nil reply: GET not given: Operation was aborted (conflict with one of the XX/NX options).
+	//		- Simple string reply: OK. GET not given: The key was set.
+	//		- Nil reply: GET given: The key didn't exist before the SET.
+	//		- Bulk string reply: GET given: The previous value of the key.
+	// RESP3 Reply:
+	//	Any of the following:
+	//		- Null reply: GET not given: Operation was aborted (conflict with one of the XX/NX options).
+	//		- Simple string reply: OK. GET not given: The key was set.
+	//		- Null reply: GET given: The key didn't exist before the SET.
+	//		- Bulk string reply: GET given: The previous value of the key.
+	// History:
+	//	- Starting with Redis version 2.6.12: Added the EX, PX, NX and XX options.
+	//	- Starting with Redis version 6.0.0: Added the KEEPTTL option.
+	//	- Starting with Redis version 6.2.0: Added the GET, EXAT and PXAT option.
+	//	- Starting with Redis version 7.0.0: Allowed the NX and GET options to be used together.
 	Set(ctx context.Context, key string, value any, expiration time.Duration) StatusCmd
+	SetXX(ctx context.Context, key string, value any, expiration time.Duration) BoolCmd
+	SetArgs(ctx context.Context, key string, value any, a SetArgs) StatusCmd
 
 	// SetEX
 	// Available since: 2.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @slow
+	// RESP2 / RESP3 Reply:
+	// 	- Simple string reply: OK.
 	SetEX(ctx context.Context, key string, value any, expiration time.Duration) StatusCmd
 
 	// SetNX
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @write @string @fast
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Integer reply: 0 if the key was not set.
+	//		- Integer reply: 1 if the key was set.
 	SetNX(ctx context.Context, key string, value any, expiration time.Duration) BoolCmd
-
-	// SetXX
-	// Available since: 1.0.0
-	// Time complexity: O(1)
-	// ACL categories: @write @string @fast
-	SetXX(ctx context.Context, key string, value any, expiration time.Duration) BoolCmd
-
-	// SetArgs
-	// Available since: 1.0.0
-	// Time complexity: O(1)
-	// ACL categories: @write @string @fast
-	SetArgs(ctx context.Context, key string, value any, a SetArgs) StatusCmd
 
 	// SetRange
 	// Available since: 2.2.0
-	// Time complexity: O(1), not counting the time taken to copy the new string in place. Usually, this string is very small so the amortized complexity is O(1). Otherwise, complexity is O(M) with M being the length of the value argument.
+	// Time complexity: O(1), not counting the time taken to copy the new string in place. Usually, this string is very small so the amortized complexity is O(1).
+	//					Otherwise, complexity is O(M) with M being the length of the value argument.
 	// ACL categories: @write @string @slow
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the length of the string after it was modified by the command.
 	SetRange(ctx context.Context, key string, offset int64, value string) IntCmd
 }
 
@@ -121,6 +183,8 @@ type StringReader interface {
 	// Available since: 1.0.0
 	// Time complexity: O(N) where N is the number of keys to retrieve.
 	// ACL categories: @read @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Array reply: a list of values at the specified keys.
 	MGet(ctx context.Context, keys ...string) SliceCmd
 }
 
@@ -129,18 +193,27 @@ type StringCacheCmdable interface {
 	// Available since: 1.0.0
 	// Time complexity: O(1)
 	// ACL categories: @read @string @fast
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Bulk string reply: the value of the key.
+	//		- Nil reply: if the key does not exist.
 	Get(ctx context.Context, key string) StringCmd
 
 	// GetRange
 	// Available since: 2.4.0
-	// Time complexity: O(N) where N is the length of the returned string. The complexity is ultimately determined by the returned length, but because creating a substring from an existing string is very cheap, it can be considered O(1) for small strings.
+	// Time complexity: O(N) where N is the length of the returned string. The complexity is ultimately determined by the returned length,
+	//					but because creating a substring from an existing string is very cheap, it can be considered O(1) for small strings.
 	// ACL categories: @read @string @slow
+	// RESP2 / RESP3 Reply:
+	// 	- Bulk string reply: The substring of the string value stored at key, determined by the offsets start and end (both are inclusive).
 	GetRange(ctx context.Context, key string, start, end int64) StringCmd
 
 	// StrLen
 	// Available since: 2.2.0
 	// Time complexity: O(1)
 	// ACL categories: @read @string @fast
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the length of the string stored at key, or 0 when the key does not exist.
 	StrLen(ctx context.Context, key string) IntCmd
 }
 
