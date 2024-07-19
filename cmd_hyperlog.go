9649275e2f44127422f9d8ab44a5cfcfe35311a5
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-07-19 15:34:26 +0800

    refactor: 注释

--- cmd_hyperlog.go
+++ cmd_hyperlog.go
@@ -14,20 +14,29 @@ type HyperLogWriter interface {
 	// Available since: 2.8.9
 	// Time complexity: O(1) to add every element.
 	// ACL categories: @write @hyperloglog @fast
+	// RESP2 / RESP3 Reply:
+	//	One of the following:
+	//		- Integer reply: 1 if at least one HyperLogLog internal register was altered.
+	//		- Integer reply: 0 if no HyperLogLog internal registers were altered.
 	PFAdd(ctx context.Context, key string, els ...any) IntCmd
 
 	// PFMerge
 	// Available since: 2.8.9
 	// Time complexity: O(N) to merge N HyperLogLogs, but with high constant times.
 	// ACL categories: @write @hyperloglog @slow
+	// RESP2 / RESP3 Reply:
+	// 	- Simple string reply: OK.
 	PFMerge(ctx context.Context, dest string, keys ...string) StatusCmd
 }
 
 type HyperLogReader interface {
 	// PFCount
 	// Available since: 2.8.9
-	// Time complexity: O(1) with a very small average constant time when called with a single key. O(N) with N being the number of keys, and much bigger constant times, when called with multiple keys.
+	// Time complexity: O(1) with a very small average constant time when called with a single key.
+	//	O(N) with N being the number of keys, and much bigger constant times, when called with multiple keys.
 	// ACL categories: @read @hyperloglog @slow
+	// RESP2 / RESP3 Reply:
+	// 	- Integer reply: the approximated number of unique elements observed via PFADD
 	PFCount(ctx context.Context, keys ...string) IntCmd
 }
 
